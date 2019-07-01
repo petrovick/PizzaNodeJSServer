ef0a0a8198375c55021f5ff5a45175c2f01cad87
Petrovick Ubuntu <petrovickg@hotmail.com>
2019-07-01 16:14:35 -0300

    Add cors and admin permission to create an admin user

--- src/app/controllers/UserAdminController.js
+++ src/app/controllers/UserAdminController.js
@@ -1,13 +1,14 @@
-const { User } = require('../models')
+const { User } = require("../models");
 
 class UserAdminController {
-  async store (req, res) {
+  async store(req, res) {
+    req.body.avatar = "avatarhere";
     if (req.isAdmin) {
-      var user = await User.create({ ...req.body, is_admin: true })
-      return res.status(200).json(user)
+      var user = await User.create({ ...req.body, is_admin: true });
+      return res.status(200).json(user);
     }
-    return res.status(401)
+    return res.status(401);
   }
 }
 
-module.exports = new UserAdminController()
+module.exports = new UserAdminController();
